5643a83e940985e0da8b35bb8db17707bde7ceae
Vladimir Hrechko <grechko_vo@navis.ru>
2022-01-12 11:20:19 +0300

    Take out Display class to another file

---
+++ display.py
@@ -0,0 +1,51 @@
+from pygame import *
+from bullet import Bullet
+import threading
+import time
+
+class Display:
+    def __init__(self, width, heigh):
+        self.__size = (width, heigh)
+        self.__bg = (0,0,0)
+        self.__sc = display.set_mode((width, heigh))
+
+    def set_bg(self, rgb):
+        self.__bg = rgb
+        self.__sc.fill(self.__bg)
+
+    def get_bg(self):
+        return self.__bg
+
+    def draw_circle(self, coord, radius, color, border):
+        draw.circle(self.__sc, color, coord, radius, border)
+        display.update()
+
+    def hide_circle(self, coord, radius, border):
+        draw.circle(self.__sc, self.__bg, coord, radius, border)
+        display.update()
+
+    def draw_line(self, coor_bgn, coor_end, color, bold):
+        draw.line(self.__sc, color, coor_bgn, coor_end, bold)
+        display.update()
+
+    def hide_line(self, coor_bgn, coor_end, bold):
+        draw.line(self.__sc, self.__bg, coor_bgn, coor_end, bold)
+        display.update()
+
+    def get_size(self):
+        return self.__size
+
+    def shooter(self, x, y, mx, my, bul_len, bul_width, bul_color):
+        th = threading.Thread(target=self.__shoot_th, args=(x,y,mx,my,bul_len,bul_width,bul_color))
+        th.start()
+
+    def __shoot_th(self, x, y, mx, my, bul_len, bul_width,bul_color):
+        bl = Bullet()
+        bl.set_len(bul_len)
+        bl.set_bgn_point(x, y)
+        bl.set_end_point(mx, my)
+        while bl.get_next_pos() != None:
+            b_line = bl.get_last_pos()
+            self.draw_line(b_line[0], b_line[1], bul_color, bul_width)
+            time.sleep(0.01)
+            self.hide_line(b_line[0], b_line[1], bul_width)
--- main.py
+++ main.py
@@ -1,99 +1,18 @@
 from pygame import *
-from body import Body
-import threading
 import time
+from body import Body
 from bullet import Bullet
 from keystorage import KeyStorage
+from display import Display
 from pprint import pprint
 
-BODY_BORDER_COLOR = (200,15,25)
-BODY_BULLET_COLOR = (20,150,25)
-BODY_BUL_CLR = (45, 65, 100)
-
-def draw_body(x1, y1, R):
-    global sc
-    draw.circle(sc, BODY_BORDER_COLOR, (y1, x1), R, 5)
-
-def hide_body(x1, y1, R):
-    global sc
-    draw.circle(sc, (0,0,0), (y1, x1), R, 5)
-
-def draw_line(x1, y1, x2, y2):
-    global sc
-    draw.line(sc, BODY_BORDER_COLOR, (y1, x1), (y2, x2), 5)
-
-def hide_line(x1, y1, x2, y2):
-    global sc
-    draw.line(sc, (0,0,0), (y1, x1), (y2, x2), 5)
-
-def shooter(x, y, mx, my):
-    th = threading.Thread(target=shoot_th, args=(x,y,mx,my))
-    th.start()
-
-def shoot_th(x, y, mx, my):
-    global sc
-    bl = Bullet()
-    bl.set_len(8)
-    bl.set_bgn_point(x, y)
-    bl.set_end_point(mx, my)
-    while bl.get_next_pos() != None:
-        b_line = bl.get_last_pos()
-        draw.line(sc, BODY_BULLET_COLOR, b_line[0], b_line[1], 5)
-        display.update()
-        time.sleep(0.01)
-        draw.line(sc, (0,0,0), b_line[0], b_line[1], 5)
-        display.update()
-
-
-class Display:
-    def __init__(self, width, heigh):
-        self.__size = (width, heigh)
-        self.__bg = (0,0,0)
-        self.__sc = display.set_mode((width, heigh))
-
-    def set_bg(self, rgb):
-        self.__bg = rgb
-        self.__sc.fill(self.__bg)
-
-    def get_bg(self):
-        return self.__bg
-
-    def draw_circle(self, coord, radius, color, border):
-        draw.circle(self.__sc, color, coord, radius, border)
-        display.update()
-
-    def hide_circle(self, coord, radius, border):
-        draw.circle(self.__sc, self.__bg, coord, radius, border)
-        display.update()
-
-    def draw_line(self, coor_bgn, coor_end, color, bold):
-        draw.line(self.__sc, color, coor_bgn, coor_end, bold)
-        display.update()
-
-    def hide_line(self, coor_bgn, coor_end, bold):
-        draw.line(self.__sc, self.__bg, coor_bgn, coor_end, bold)
-        display.update()
-
-    def get_size(self):
-        return self.__size
-
-    def shooter(self, x, y, mx, my, bul_len, bul_width, bul_color):
-        th = threading.Thread(target=self.__shoot_th, args=(x,y,mx,my,bul_len,bul_width,bul_color))
-        th.start()
-
-    def __shoot_th(self, x, y, mx, my, bul_len, bul_width,bul_color):
-        bl = Bullet()
-        bl.set_len(bul_len)
-        bl.set_bgn_point(x, y)
-        bl.set_end_point(mx, my)
-        while bl.get_next_pos() != None:
-            b_line = bl.get_last_pos()
-            self.draw_line(b_line[0], b_line[1], bul_color, bul_width)
-            time.sleep(0.01)
-            self.hide_line(b_line[0], b_line[1], bul_width)
+BODY_BORDER_COLOR = (0,0,0)
+BODY_BUL_CLR = (255, 255, 255)
+DISPLAY_BG_COLOR = (100, 100, 100)
 
 # Main Body
 disp = Display(1024, 600)
+disp.set_bg(DISPLAY_BG_COLOR)
 
 obj = Body(25, disp.get_size(), BODY_BORDER_COLOR, 2)
 key_bindings = {
@@ -107,8 +26,8 @@ obj.set_draw(disp.draw_circle)
 obj.set_hide(disp.hide_circle)
 obj.set_draw_line(disp.draw_line)
 obj.set_hide_line(disp.hide_line)
-obj.set_bullet_prm(4, 3, BODY_BUL_CLR)
 obj.set_shooter(disp.shooter)
+obj.set_bullet_prm(4, 3, BODY_BUL_CLR)
 obj.show()
 
 while 1:
@@ -125,6 +44,4 @@ while 1:
         elif ev.type == MOUSEBUTTONDOWN:
             ms = mouse.get_pos()
             obj.shoot(ms[1], ms[0])
-        #else:
-        #    pass
     time.sleep(0.005)
