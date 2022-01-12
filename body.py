cf976ef619d3e3efa062b08ad77a04a7bc019f8f
BadLagger <lanynamer1985@gmail.com>
2022-01-12 07:41:15 +0300

    Refactoring: create Display class, extend KeyStorage

--- body.py
+++ body.py
@@ -1,10 +1,12 @@
 class Body:
-    def __init__(self, x=0, y=0, R=0, max_x=0, max_y=0):
-        self.__R  = R
-        self.__x1 = x
-        self.__y1 = y
-        self.__max_x = max_x
-        self.__max_y = max_y
+    def __init__(self, radius, size, color, border_size):
+        self.__R  = radius
+        self.__color = color
+        self.__br_size = border_size
+        self.__x1 = size[0] / 2
+        self.__y1 = size[1] / 2
+        self.__max_x = size[0]
+        self.__max_y = size[1]
         self.__x2 = 0
         self.__y2 = 0
         self.__step = 10
@@ -40,20 +42,20 @@ class Body:
             if self.__R:
                 if not self.__visible:
                     self.__visible = True
-                    self.__draw(self.__x1, self.__y1, self.__R)
-                    self.__draw_line(self.__x1, self.__y1, self.__x2, self.__y2)
+                    self.__draw((self.__x1, self.__y1), self.__R, self.__color, self.__br_size)
+                    self.__draw_line((self.__x1, self.__y1), (self.__x2, self.__y2), self.__color, self.__br_size)
 
     def hide(self):
         if self.__hide and self.__hide_line:
             if self.__visible:
                 self.__visible = False
-                self.__hide(self.__x1, self.__y1, self.__R)
-                self.__hide_line(self.__x1, self.__y1, self.__x2, self.__y2)
+                self.__hide((self.__x1, self.__y1), self.__R, self.__br_size)
+                self.__hide_line((self.__x1, self.__y1), (self.__x2, self.__y2), self.__br_size)
 
-    def rotate(self, x, y):
+    def rotate(self, coord):
         if self.__draw_line and self.__hide_line and self.__visible:
             self.hide()
-            self.calc_x2_pos(x, y)
+            self.calc_x2_pos(coord[0], coord[1])
             self.show()
 
     def move_up(self):
--- main.py
+++ main.py
@@ -43,87 +43,115 @@ def shoot_th(x, y, mx, my):
         draw.line(sc, (0,0,0), b_line[0], b_line[1], 5)
         display.update()
 
-def key_bindings(key):
-    global obj
-    if key == K_ESCAPE:
-        if obj.isvisible():
-            obj.hide()
-        else:
-            obj.show()
-    elif key == K_UP:
-        obj.move_up()
-    elif key == K_DOWN:
-        obj.move_down()
-    elif key == K_RIGHT:
-        obj.move_right()
-    elif key == K_LEFT:
-        obj.move_left()
-
-def key_process(key):
-    key_bindings(key)
-    display.update()
-
 def key_watch(args):
     while args["esc"] == False:
         args["call"]()
         time.sleep(0.1)
 
 class KeyStorage:
-    def __init__(self, ext_handler):
+    def __init__(self, key_bind):
         self.__key_array = []
-        self.__callback = ext_handler
+        self.__key_th_arg = {}
+        self.__key_th_arg["esc"] = False
+        self.__key_th = threading.Thread(target=self.__thread, args=(self.__key_th_arg,))
+        self.__key_th.start()
+        self.__key_bind = key_bind
 
     def add_key(self, key):
         self.__key_array.append(key)
+        self.__process(key)
 
     def remove_key(self, key):
         if self.__key_array.count(key) != 0:
             self.__key_array.remove(key)
+    
+    def shutdown(self):
+        self.__key_th_arg["esc"] = True
+        self.__key_th.join()
 
-    def check_keys(self):
+    def __check_keys(self):
         if len(self.__key_array):
             for key in self.__key_array:
-                self.__callback(key)
+                self.__process(key)
+                
+    def __thread(self, args):
+        while args["esc"] == False:
+            self.__check_keys()
+            time.sleep(0.1)
+    
+    def __process(self, key):
+        for k in self.__key_bind:
+            if k == key:
+                self.__key_bind[k]()
+                break
+            
+
+class Display:
+    def __init__(self, width, heigh):
+        self.__size = (width, heigh)
+        self.__bg = (0,0,0)
+        self.__sc = display.set_mode((width, heigh))
+    
+    def set_bg(self, rgb):
+        self.__bg = rgb
+        self.__sc.fill(self.__bg)
+    
+    def get_bg(self):
+        return self.__bg
+    
+    def draw_circle(self, coord, radius, color, border):
+        draw.circle(self.__sc, color, coord, radius, border)
+        display.update()
+    
+    def hide_circle(self, coord, radius, border):
+        draw.circle(self.__sc, self.__bg, coord, radius, border)
+        display.update()
+    
+    def draw_line(self, coor_bgn, coor_end, color, bold):
+        draw.line(self.__sc, color, coor_bgn, coor_end, bold)
+        display.update()
+    
+    def hide_line(self, coor_bgn, coor_end, bold):
+        draw.line(self.__sc, self.__bg, coor_bgn, coor_end, bold)
+        display.update()
+ 
+    def get_size(self):
+        return self.__size
+    
+    #def shooter(self, )
 
 # Main Body
-sc = display.set_mode((800, 600))
-ks = KeyStorage(key_process)
+disp = Display(800, 600)
 
-obj = Body(300, 400, 50, 600, 800)
-obj.set_draw(draw_body)
-obj.set_hide(hide_body)
-obj.set_draw_line(draw_line)
-obj.set_hide_line(hide_line)
-obj.set_shooter(shooter)
+obj = Body(25, disp.get_size(), BODY_BORDER_COLOR, 2)
+key_bindings = {
+    K_UP: obj.move_up,
+    K_DOWN: obj.move_down,
+    K_LEFT: obj.move_left,
+    K_RIGHT: obj.move_right
+}
+ks = KeyStorage(key_bindings)
+obj.set_draw(disp.draw_circle)
+obj.set_hide(disp.hide_circle)
+obj.set_draw_line(disp.draw_line)
+obj.set_hide_line(disp.hide_line)
+#obj.set_shooter(shooter)
 obj.show()
-
-display.update()
-th_args = {}
-th_args["esc"] = False
-th_args["call"] = ks.check_keys
-th = threading.Thread(target=key_watch, args=(th_args,))
-th.start()
 
 while 1:
     for ev in event.get():
         if ev.type == QUIT:
-            th_args["esc"] = True
-            th.join()
+            ks.shutdown()
             exit()
         elif ev.type == KEYDOWN:
             ks.add_key(ev.key)
-            key_process(ev.key)
         elif ev.type == KEYUP:
-            #print('key up: ', ev.key)
             ks.remove_key(ev.key)
         elif ev.type == MOUSEMOTION:
-            ms = mouse.get_pos()
-            obj.rotate(ms[1], ms[0])
-            display.update()
-        elif ev.type == MOUSEBUTTONDOWN:
-            ms = mouse.get_pos()
-            obj.shoot(ms[1], ms[0])
-        else:
-            pass
+            obj.rotate(mouse.get_pos())
+        #elif ev.type == MOUSEBUTTONDOWN:
+        #    ms = mouse.get_pos()
+        #    obj.shoot(ms[1], ms[0])
+        #else:
+        #    pass
     time.sleep(0.005)
-    #time.delay(100)
